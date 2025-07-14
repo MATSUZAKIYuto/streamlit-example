bdf9463124118abafc6a9f83c168d3ab103a1f7e
Yuto Matsuzaki <FCA2306070026@edu.fca.ac.jp>
2025-07-14 18:24:01 +0900

    streamlitに対するテスト、変更したものが反映されるのかどうか

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # streamlit-example
 
 授業中に作成
+
+https://streamlit.io/cloud
--- simple_app.py
+++ simple_app.py
@@ -20,6 +20,6 @@ age = st.slider("年齢を選んでください", 0, 100, 25)
 # 結果表示
 if name:    # nameという変数が空(null)ではなかったら
     st.write(f"こんにちは、{name}さん！ あなたは{age}歳ですね。")
-    st.write(f"{age-5}歳にしか見えないですけど")
+    st.write(f"{age+10}歳にしか見えないですけど")
 else:
     st.write("上の入力欄に名前を入力してください。")
